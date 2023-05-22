2d894d78fd4cf9f98d25e1fa1e199550e189780e
JoelMndz <luisjo3lml@gmail.com>
2023-05-22 14:36:24 -0500

    Capa modelo

---
+++ app/src/main/java/com/example/pruebakotlin/model/DummyData.kt
@@ -0,0 +1,6 @@
+package com.example.pruebakotlin.model
+
+var movies = mutableListOf(
+    MovieModel("Star wards","Juan Villa"),
+    MovieModel("Harry Potter","Maria salvatierra")
+)
---
+++ app/src/main/java/com/example/pruebakotlin/model/MovieModel.kt
@@ -0,0 +1,6 @@
+package com.example.pruebakotlin.model
+
+data class MovieModel(
+    var titulo:String,
+    var director: String
+)
